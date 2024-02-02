a2fc98ce8ce907a41974893efd96aadd092b706f
Sifatsb <sifatsb1906@gmail.com>
2024-02-02 10:38:43 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/apple_gadget_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apple_gadget_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/splash/bindings/splash_binding.dart';
+import '../modules/splash/views/splash_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+    ),
+  ];
+}
